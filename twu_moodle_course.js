4ee7dd842ac714224e4fdbf8443b1aff7046de20
inoklim-twu <inok.lim@twu.ca>
2025-01-13 11:59:14 -0800

    TWU Custom JavaScript file to include in Moodle

---
+++ twu_moodle_course.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $("select").change(function () {
+    $(this).find("option:selected").each(function () {
+      var optionValue = $(this).attr("value");
+      if (optionValue) {
+        $(".toggle-box").not("." + optionValue).hide();
+        $("." + optionValue).show();
+      } else {
+        $(".toggle-box").hide();
+      }
+    });
+  }).change();
+});
