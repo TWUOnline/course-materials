fe127bd52c5b4ab87f6d77d8a7ad0789d8a3a13b
Inok Lim <162205836+inoklim-twu@users.noreply.github.com>
2025-01-15 15:09:35 -0800

    Update twu_moodle_course.js

--- twu_moodle_course.js
+++ twu_moodle_course.js
@@ -1,11 +1,6 @@
 $(document).ready(function () {
     $("select").change(function () {
         $(this).find("option:selected").each(function () {
-            const iframes = document.querySelectorAll("iframe");
-            iframes.forEach((videoframe) => {
-                const iframePlayer = new Vimeo.Player(videoframe);
-                iframePlayer.pause();
-            });
             var optionValue = $(this).attr("value");
             if (optionValue) {
                 $(".toggle-box").not("." + optionValue).hide();
